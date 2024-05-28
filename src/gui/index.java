a643dabdafa2e5668b7a2de9758e419fe9664b8d
shash <shash@Isuru>
2024-05-28 22:17:39 +0530

    index

--- src/gui/index.java
+++ src/gui/index.java
@@ -81,6 +81,7 @@ public class index extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
